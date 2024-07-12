c2c648199c367b4efbadc87ed6df75fe530e24b3
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-07-12 11:02:40 -0400

    trying another deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 
