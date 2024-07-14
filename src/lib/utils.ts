c140b729b7513d72d2f12f7a66d127687023a68d
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-07-14 13:07:49 -0400

    add utility to handle promises

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export async function tc<T>(
+  promise: Promise<T>,
+): Promise<{ data: T | null; error: Error | null }> {
+  try {
+    const data = await promise;
+    return { data, error: null };
+  } catch (throwable) {
+    if (throwable instanceof Error) return { data: null, error: throwable };
+    throw throwable;
+  }
+}
