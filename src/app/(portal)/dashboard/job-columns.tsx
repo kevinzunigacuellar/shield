b345040673fc7cbecd7a6625b383384022eb8366
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-07-13 21:43:45 -0400

    wip: dialog

---
+++ src/app/(portal)/dashboard/job-columns.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import { MoreHorizontal } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { DataTableColumnHeader } from "@/components/column-header";
+import { deleteJob } from "../job/actions";
+import Link from "next/link";
+
+type Job = {
+  id: string;
+  title: string;
+  xata_createdat: Date;
+};
+
+export const columns: ColumnDef<Job>[] = [
+  {
+    accessorKey: "title",
+    header: "Title",
+    cell: ({ row }) => {
+      const job = row.original;
+      return (
+        <Link
+          href={`/jobs/applications/${job.id}`}
+          className="underline-offset-4 hover:underline"
+        >
+          {job.title}
+        </Link>
+      );
+    },
+  },
+  {
+    accessorKey: "xata_createdat",
+    header: ({ column }) => {
+      return <DataTableColumnHeader column={column} title="Created at" />;
+    },
+    cell: ({ row }) => {
+      const job = row.original;
+      return (
+        <span>
+          {job.xata_createdat.toLocaleString("en-US", {
+            dateStyle: "medium",
+          })}
+        </span>
+      );
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const job = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem asChild>
+              <Link href={`/job/edit/${job.id}`}>Edit</Link>
+            </DropdownMenuItem>
+            <Dialog>
+              <DialogTrigger asChild>
+                <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+                  Delete
+                </DropdownMenuItem>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-md">
+                <DialogHeader>
+                  <DialogTitle>Delete job</DialogTitle>
+                  <DialogDescription>
+                    Are you sure you want to permanently delete this job? This
+                    action cannot be undone.
+                  </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button type="button" variant="outline">
+                      Cancel
+                    </Button>
+                  </DialogClose>
+                  <DialogClose asChild>
+                    <Button
+                      onClick={async () => {
+                        await deleteJob(job.id);
+                      }}
+                    >
+                      Delete
+                    </Button>
+                  </DialogClose>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem asChild>
+              <Link href={`/post/${job.id}`}>Live preview</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
