0f8daf05d41234725ba928a4be0cd59f064aa295
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-07-20 18:59:36 -0400

    replace backend upload thing sdk call with endpoint

---
+++ src/lib/uploadthing/core.ts
@@ -0,0 +1,18 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const fileRouter = {
+  upload: f({
+    pdf: {
+      maxFileCount: 1,
+      maxFileSize: "1MB",
+      minFileCount: 1,
+    },
+  }).onUploadComplete(async ({ file }) => {
+    const { size, url, name } = file;
+    return { size, url, name };
+  }),
+} satisfies FileRouter;
+
+export type FileRouterT = typeof fileRouter;
