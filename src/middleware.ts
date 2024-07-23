f939a89310a3b02653f0723b89205960c2b0f1e4
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-07-22 22:16:53 -0400

    fix: 404 on signout

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,16 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher(["/jobs(.*)", "/applications(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  if (isProtectedRoute(req)) {
+    const { userId } = auth();
+    if (!userId) {
+      return auth().redirectToSignIn({
+        returnBackUrl: req.url,
+      });
+    }
+
+    auth().protect();
+  }
 });
 
 export const config = {
