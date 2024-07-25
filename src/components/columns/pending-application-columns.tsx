230df1adabf95058e792568b700719d4188ee72e
Kevin <46791833+kevinzunigacuellar@users.noreply.github.com>
2024-07-24 22:29:15 -0400

    add radial chart applications table score cell (#14)

--- src/components/columns/pending-application-columns.tsx
+++ src/components/columns/pending-application-columns.tsx
@@ -16,6 +16,14 @@ import { toast } from "sonner";
 import Link from "next/link";
 import { rejectApplication } from "@/actions/application";
 import type { ApplicationType } from "@/schema/application";
+import {
+  Label,
+  PolarGrid,
+  PolarRadiusAxis,
+  RadialBar,
+  RadialBarChart,
+} from "recharts";
+import { ChartConfig, ChartContainer } from "@/components/ui/chart";
 
 export const columns: ColumnDef<ApplicationType>[] = [
   {
@@ -51,6 +59,79 @@ export const columns: ColumnDef<ApplicationType>[] = [
     accessorKey: "score",
     header: ({ column }) => {
       return <DataTableColumnHeader column={column} title="Score" />;
+    },
+    cell: ({ row }) => {
+      const application = row.original;
+
+      const getRingColor = (score = 10) => {
+        if (score >= 90) {
+          return "hsl(var(--chart-2))";
+        } else if (score >= 70) {
+          return "hsl(var(--chart-4))";
+        } else if (score >= 50) {
+          return "hsl(var(--chart-5))";
+        } else {
+          return "hsl(var(--chart-1))";
+        }
+      };
+      const chartData = [
+        { score: application.score!, fill: "var(--color-score)" },
+      ];
+      const chartConfig = {
+        score: {
+          label: "Score",
+          color: getRingColor(application.score),
+        },
+      } satisfies ChartConfig;
+      return (
+        <ChartContainer config={chartConfig} className="aspect-square max-h-10">
+          <RadialBarChart
+            data={chartData}
+            startAngle={0}
+            endAngle={chartData[0].score * 3.6}
+            innerRadius={15}
+            outerRadius={25}
+          >
+            <PolarGrid
+              gridType="circle"
+              radialLines={false}
+              stroke="none"
+              className="first:fill-muted last:fill-background"
+              polarRadius={[17, 13]}
+            />
+            <RadialBar dataKey="score" background cornerRadius={10} />
+            <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-foreground text-xs font-semibold"
+                        >
+                          {chartData[0].score.toLocaleString()}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 24}
+                          className="fill-muted-foreground"
+                        ></tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
+            </PolarRadiusAxis>
+          </RadialBarChart>
+        </ChartContainer>
+      );
     },
   },
   {
