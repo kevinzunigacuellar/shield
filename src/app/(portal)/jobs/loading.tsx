158d219693c93333b145ff3bad58c4f2af228fd0
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-08-02 18:54:35 -0400

    add loading and error boundaries

---
+++ src/app/(portal)/jobs/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { LoaderCircle } from "lucide-react";
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="flex w-full h-full flex-1 flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
+      <LoaderCircle className="size-10 animate-spin" />
+    </div>
+  );
+}
